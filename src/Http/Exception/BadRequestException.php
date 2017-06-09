698c3b73581a9703a9c932890c57e50f8774607a
velkuns <romain.inscription@hotmail.fr>
2017-06-09 09:20:06 +0200

    Add new exception file

---
+++ src/Http/Exception/BadRequestException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright (c) 2010-2017 Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eureka\Component\Http\Exception;
+
+/**
+ * Class BadRequestException
+ *
+ * @author  Romain Cottard
+ */
+class BadRequestException extends \Exception
+{
+}
